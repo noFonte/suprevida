9e52db53161774c13487a5e4963f5a5cb5267dab
AnalistivsData <110004930+anlisticsData@users.noreply.github.com>
2024-10-23 20:52:44 -0300

    start do projeto

---
+++ src/main/java/com/suprevida/suprevida/SuprevidaApplication.java
@@ -0,0 +1,13 @@
+package com.suprevida.suprevida;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuprevidaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuprevidaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/suprevida/suprevida/SuprevidaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suprevida.suprevida;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuprevidaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
