e2aba0e516c8cbad5f02df55226b984536c5a58a
AnalistivsData <110004930+anlisticsData@users.noreply.github.com>
2024-10-24 21:35:22 -0300

    Crud de Produtos Finalizados

--- src/main/java/com/suprevida/suprevida/useCases/DeleteProductByUseCase.java
+++ src/main/java/com/suprevida/suprevida/useCases/DeleteProductByUseCase.java
@@ -1,24 +1,17 @@
 package com.suprevida.suprevida.useCases;
 
-import com.suprevida.suprevida.entyties.ProductEntity;
 import com.suprevida.suprevida.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 @Service
 public class DeleteProductByUseCase {
     @Autowired
     private ProductRepository productRepository;
-
-
-
     public boolean execute(Long id) {
-
         if(productRepository.existsById(id)){
             productRepository.deleteById(id);
             return true;
         }
         return false;
-
     }
 }
